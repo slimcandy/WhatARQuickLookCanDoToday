57ba30a2afff5f7b4e8caf4e63e5bc7bdef28e04
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-12-18 21:13:07 +0100

    Add ar banners for each object

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,42 @@
-const linkElement = document.getElementById('room-plan-ar-link');
+// Room request room tour.
+const roomLinkElement = document.getElementById('room-plan-ar-link');
 
-const handleMessage = (event: MessageEvent) => {
+const roomHandleMessage = (event: MessageEvent) => {
   if (event.data === '_apple_ar_quicklook_button_tapped') {
     // Handle the user tap.
-    window.location.href = 'tel:+2306989800';
+    window.location.href =
+      'mailto:room-tour@extremely-luxury.apartment?subject=%D0%97%D0%B0%D0%BF%D0%B8%D1%81%D1%8C%20%D0%BD%D0%B0%20%D0%BF%D1%80%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80&body=%D0%94%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B4%D0%B5%D0%BD%D1%8C!%0A%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%D1%81%D1%8F%20%D0%BD%D0%B0%20%D0%BF%D1%80%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B.%20%0A%0A%D0%96%D0%9A%20%D0%92%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9%20%D0%BD%D0%B0%20%D0%A2%D0%B0%D0%BB%D0%B8%D1%81%D0%BC%D0%B0%D0%BD%D0%B5%0A%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D0%A1%D0%90%D0%9E%2C%20%D1%80-%D0%BD%20%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%2C%20%D0%BC.%20%D0%A1%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9%20%D0%B2%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9%2C%20%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%BE%D0%B5%20%D1%88%D0%BE%D1%81%D1%81%D0%B5%2011';
   }
 };
 
-if (linkElement) {
-  linkElement.addEventListener('message', handleMessage, false, true);
+if (roomLinkElement) {
+  roomLinkElement.addEventListener('message', roomHandleMessage, false);
+}
+
+// School uniform call.
+const schoolLinkElement = document.getElementById('school-ar-link');
+
+const schoolHandleMessage = (event: MessageEvent) => {
+  if (event.data === '_apple_ar_quicklook_button_tapped') {
+    // Handle the user tap.
+    window.location.href = 'tel:+18554667467';
+  }
+};
+
+if (schoolLinkElement) {
+  schoolLinkElement.addEventListener('message', schoolHandleMessage, false);
+}
+
+// Dino wiki page.
+const dinoLinkElement = document.getElementById('dino-ar-link');
+
+const dinoHandleMessage = (event: MessageEvent) => {
+  if (event.data === '_apple_ar_quicklook_button_tapped') {
+    // Handle the user tap.
+    window.location.href = 'https://en.wikipedia.org/wiki/Stegosaurus';
+  }
+};
+
+if (dinoLinkElement) {
+  dinoLinkElement.addEventListener('message', dinoHandleMessage, false);
 }
