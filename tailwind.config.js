cbf37b43c8d73cc60b12348e9c8494a90c82dbe8
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-12-25 21:13:22 +0100

    Add Halloween expample

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
       colors: {
         'custom-grey': '#D9D9D93D',
         'custom-link': '#0066CC',
-        'custom-dark-grey': '#6e6e73'
+        'custom-dark-grey': '#6e6e73',
+        'custom-yellow': '#fcff00',
+        'custom-white': '#4e4e4eba',
+        'custom-white-shadow': '#9b9b9b'
+      },
+      backgroundImage: {
+        'ar-glyph': "url('/src/images/ARKit-glyph.png')"
       }
     }
   },
