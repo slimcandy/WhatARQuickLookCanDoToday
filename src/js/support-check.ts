7a47e36ba02e9a97e15b24ec83972b7b2562349f
SlimCandy <cgpnrjmhppeb@ro.ru>
2022-12-27 20:09:01 +0100

    Add compatibility check

---
+++ src/js/support-check.ts
@@ -0,0 +1,12 @@
+const headerForNoAR = document.getElementById(
+  'header-for-no-ar'
+) as HTMLAnchorElement;
+const headerForAR = document.getElementById(
+  'header-for-ar'
+) as HTMLAnchorElement;
+
+const a: HTMLAnchorElement = document.createElement('a');
+if (!a.relList.supports('ar') && headerForNoAR && headerForAR) {
+  headerForAR.classList.add('hidden');
+  headerForNoAR.classList.remove('hidden');
+}
